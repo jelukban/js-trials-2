19c19695ba36b50e24ccb027d8d393fcc3345129
Jane Tang <jane.tang118@gmail.com>
2022-09-24 16:41:43 -0700

    Completed objects.js

--- objects.js
+++ objects.js
@@ -6,11 +6,7 @@ function countWords(phrase) {
 
   for (let word of phrase.split(' ')) {
    
-    if (word_counts.hasOwnProperty(word)) {
-      word_counts[word] += 1;
-    } else {
-      word_counts[word] = 1;
-    }
+    word_counts.hasOwnProperty(word) ? word_counts[word] += 1 : word_counts[word] = 1;
   }
   return word_counts;
 }
