0b500511d3cd658150593759f543d2df3c58e62d
Jane Tang <jane.tang118@gmail.com>
2022-09-24 16:29:25 -0700

    Completed objects.js

---
+++ objects.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// 1. countWords
+function countWords(phrase) {
+  const word_counts = {};
+
+  for (let word of phrase.split(' ')) {
+   
+    if (word_counts.hasOwnProperty(word)) {
+      word_counts[word] += 1;
+    } else {
+      word_counts[word] = 1;
+    }
+  }
+  return word_counts;
+}
+
+// 2. getMelonsAtPrice
+function getMelonsAtPrice(price) {
+  
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  }
+
+  if (!(melonPrices.hasOwnProperty(price))) {
+    return null
+  }
+
+  return melonPrices[price].sort();
+
+}
